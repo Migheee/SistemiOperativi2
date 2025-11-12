a4f21d0b539bab7eba59803bdd9d81bffd3864b2
Migheee <mighecaste@gmail.com>
2025-11-12 11:17:27 +0100

    Queue Messages: Introduction & Exercises

---
+++ lab/code_messaggi/ese_3/build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mighe/university/sistemiOperativi2/lab/code_messaggi/ese_3/src/client.c" "CMakeFiles/client.dir/src/client.c.o" "gcc" "CMakeFiles/client.dir/src/client.c.o.d"
+  "/home/mighe/university/sistemiOperativi2/lab/code_messaggi/ese_3/src/errExit.c" "CMakeFiles/client.dir/src/errExit.c.o" "gcc" "CMakeFiles/client.dir/src/errExit.c.o.d"
+  "/home/mighe/university/sistemiOperativi2/lab/code_messaggi/ese_3/src/order.c" "CMakeFiles/client.dir/src/order.c.o" "gcc" "CMakeFiles/client.dir/src/order.c.o.d"
+  "" "client" "gcc" "CMakeFiles/client.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
